23161ab5fc4be8a25f272712bf645d9552d27a76
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2021-10-14 18:32:02 +0900

    Fix deploy script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ set -eu
 readonly SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 source ${SCRIPT_DIR}/config.sh ${1-dev}
 
+flutter clean && flutter pub get
+
 ${SCRIPT_DIR}/build_graphql.sh ${ENV}
 
 flutter run --dart-define=FLAVOR=${ENV}
--- scripts/build_graphql.sh
+++ scripts/build_graphql.sh
@@ -5,14 +5,16 @@ set -eu
 readonly SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 source ${SCRIPT_DIR}/config.sh ${1-dev}
 
-rm -rf ${SCRIPT_DIR}/../lib/graphql/__generated__/*
+setopt +o nomatch
+cd ${SCRIPT_DIR}/../lib/graphql && ls ./ | grep -v -E 'todo.query.graphql' | xargs rm -rf 
+mkdir ${SCRIPT_DIR}/../lib/graphql/__generated__
+cp ${SCRIPT_DIR}/../lib/graphql/todo.query.graphql ${SCRIPT_DIR}/../lib/graphql/__generated__/
 rm -rf ${SCRIPT_DIR}/../lib/graphql/schema.graphql
-cp ${SCRIPT_DIR}/../lib/graphql/todo.query.graphql ${SCRIPT_DIR}/../lib/graphql/__generated__/todo.query.graphql
+mv ${SCRIPT_DIR}/../lib/graphql/todo.query.graphql ${SCRIPT_DIR}/../lib/graphql/__generated__/todo.query.graphql
 
 ${SCRIPT_DIR}/get-graph-ql.sh
 
-flutter pub run build_runner build
-
+flutter pub run build_runner build --delete-conflicting-outputs
 
 mv ${SCRIPT_DIR}/../lib/graphql/__generated__/schema.graphql ${SCRIPT_DIR}/../lib/graphql/schema.graphql
 mv ${SCRIPT_DIR}/../lib/graphql/__generated__/todo.query.graphql ${SCRIPT_DIR}/../lib/graphql/todo.query.graphql
